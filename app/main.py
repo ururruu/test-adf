f3af865e2f2ef487bc004495dff7f31c65937198
Kateryna Dubinina <ururruu@gmail.com>
2024-05-31 13:45:26 +0300

    Remove file dir

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 import os
 
 
-def main(filename: str, file_dir: str = 'files'):
-    with open(f'{file_dir}/{filename}', 'r') as file:
+def main(filename: str):
+    with open(f'files/{filename}', 'r') as file:
         print(file.read())
-    with open(f'{file_dir}/output_file.txt', 'w') as output_file:
+    with open(f'files/output_file.txt', 'w') as output_file:
         output_file.write('Test')
 
 
 if __name__ == "__main__":
     filename = os.environ['input_file']
-    file_dir = os.environ['file_dir']
-    main(filename, file_dir)
+    main(filename)
