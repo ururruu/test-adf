60a2b46d13894296a8e7e76528ffe79cd2a237a8
Kateryna Dubinina <ururruu@gmail.com>
2024-05-31 13:44:25 +0300

    add files read write

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
-def main():
-    pass
+import os
+
+
+def main(filename: str, file_dir: str = 'files'):
+    with open(f'{file_dir}/{filename}', 'r') as file:
+        print(file.read())
+    with open(f'{file_dir}/output_file.txt', 'w') as output_file:
+        output_file.write('Test')
 
 
 if __name__ == "__main__":
-    main()
+    filename = os.environ['input_file']
+    file_dir = os.environ['file_dir']
+    main(filename, file_dir)
