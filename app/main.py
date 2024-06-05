6eba049876ca20289f25132ee1775d275a19f1fd
Kateryna Dubinina <ururruu@gmail.com>
2024-06-05 13:17:24 +0300

    add files folder as input

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
+import logging
 import os
 
 
-def main(filename: str):
-    with open(f'files/{filename}', 'r') as file:
+def main(files_folder: str, filename: str):
+    with open(f'{files_folder}/{filename}', 'r') as file:
         print(file.read())
-    with open(f'files/output_file.txt', 'w') as output_file:
+    with open(f'{files_folder}/output_file.txt', 'w') as output_file:
         output_file.write('Test')
 
 
 if __name__ == "__main__":
+    print('Test')
+    logging.info('test2')
     filename = os.environ['input_file']
-    main(filename)
+    files_folder = os.environ['files_folder']
+    main(files_folder, filename)
