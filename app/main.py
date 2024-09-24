016d94c4b58d914c63cc3ad70d925210757860dc
Kateryna Dubinina <ururruu@gmail.com>
2024-09-24 15:37:09 +0300

    test

--- app/main.py
+++ app/main.py
@@ -3,15 +3,14 @@ import subprocess
 
 
 def main(filename: str):
-    # with open(f'app/files/{filename}', 'r') as file:
-    #     print(file.read())
-    with open(f'app/files/output_file.txt', 'w') as output_file:
+    with open(f'files/{filename}', 'r') as file:
+        print(file.read())
+    with open(f'files/output_file.txt', 'w') as output_file:
         output_file.write('Test')
 
 
 if __name__ == "__main__":
-    print('Test')
-    print('Test2')
+    print('Logging test')
     result = subprocess.run(['find', '.'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     print(result.stdout)
     filename = os.environ['input_file']
